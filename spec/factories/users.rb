d49d85a1c39bb4c28dd534f6a08d1719705e5a4b
Kyle Thompson <kyle@kylekthompson.com>
2017-05-23 19:50:17 -0400

    rails g model user

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    email { FFaker::Internet.email }
+    password { FFaker::Internet.password }
+    password_confirmation { password }
+  end
+end

74054f82f27d1ce1ecdbc20db130e203a30c3e47
Kyle Thompson <kyle@kylekthompson.com>
2017-05-23 19:57:53 -0400

    add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to allow_value('a@b.c').for(:email) }
+  it { is_expected.to allow_value('a.b.c@d.e').for(:email) }
+  it { is_expected.not_to allow_value('a.b').for(:email) }
+  it { is_expected.to have_secure_password }
+
+  describe '#full_name' do
+    specify { expect(subject.full_name).to eq("#{subject.first_name} #{subject.last_name}") }
+  end
 end

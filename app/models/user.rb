7ab840521ed9fb4e6a122a13fa24b18c61c0cecf
Kyle Thompson <kyle@kylekthompson.com>
2017-05-23 20:03:00 -0400

    add validations and full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, format: {
+    with: /\A.+@.+\..+\z/,
+    message: 'must be an email address'
+  }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end

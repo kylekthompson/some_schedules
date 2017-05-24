4ec308fb4ffe56b6d4f10c29682cd0e4b93378ab
Kyle Thompson <kyle@kylekthompson.com>
2017-05-23 20:11:52 -0400

    add comments and use a named subject

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+##
+# The User model represents any user of the application
 class User < ApplicationRecord
   has_secure_password
 
@@ -9,6 +11,11 @@ class User < ApplicationRecord
     message: 'must be an email address'
   }
 
+  ##
+  # Returns the full name of the user
+  #
+  # [1] pry(main)> User.new(first_name: 'Kyle', last_name: 'Thompson').full_name
+  # => "Kyle Thompson"
   def full_name
     "#{first_name} #{last_name}"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { build(:user) }
+  subject(:user) { build(:user) }
 
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
@@ -12,6 +12,6 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_secure_password }
 
   describe '#full_name' do
-    specify { expect(subject.full_name).to eq("#{subject.first_name} #{subject.last_name}") }
+    specify { expect(user.full_name).to eq("#{user.first_name} #{user.last_name}") }
   end
 end

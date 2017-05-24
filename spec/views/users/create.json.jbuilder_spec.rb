f1ff52e56a93d68ea07dab326d757f351a8259f9
Kyle Thompson <kyle@kylekthompson.com>
2017-05-23 21:47:09 -0400

    clean up view test

--- spec/views/users/create.json.jbuilder_spec.rb
+++ spec/views/users/create.json.jbuilder_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe 'users/create.json.jbuilder', type: :view do
-  let(:user) { create(:user) }
-
   subject(:rendered_user) { JSON.parse(rendered).deep_symbolize_keys }
+
+  let(:user) { create(:user) }
+  let(:expected_hash) do
+    {
+      user: {
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email
+      }
+    }
+  end
 
   before do
     assign(:user, user)
@@ -11,13 +21,6 @@ describe 'users/create.json.jbuilder', type: :view do
   end
 
   it 'properly renders the user' do
-    expect(rendered_user).to eq({
-      user: {
-        id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email
-      }
-    })
+    expect(rendered_user).to eq(expected_hash)
   end
 end

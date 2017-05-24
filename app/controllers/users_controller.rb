6d886787f628c9d4a9672e1c9153ed9846711bdf
Kyle Thompson <kyle@kylekthompson.com>
2017-05-23 21:24:02 -0400

    add users controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
-  def create; end
+  def create
+    @user = User.new(create_params)
+
+    if @user.save
+      render :create, status: :created
+    else
+      render json: { errors: @user.errors }, status: :conflict
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end

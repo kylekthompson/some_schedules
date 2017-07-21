2e20c799daaaab9d1ba666c263b979fd7f5283ee
Kyle Thompson <kyle@kylekthompson.com>
2017-07-20 20:22:11 -0700

    add test for creating tokens

---
+++ spec/requests/api/v1/user_token_controller_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::UserTokenController, type: :request do
+  describe 'POST #create' do
+    let(:request) do
+      post('/api/v1/user_token', params: { auth: auth_params }.to_json, headers: unauthenticated_headers)
+    end
+
+    context 'when passed invalid params' do
+      let(:auth_params) { { email: 'test@example.com', password: 'password' } }
+
+      it 'has the right status' do
+        request
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'when passed valid params' do
+      let(:user) { create(:user, password: 'password') }
+      let(:auth_params) { { email: user.email, password: 'password' } }
+
+      it 'has the right status' do
+        request
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns a token' do
+        request
+        expect(JSON.parse(response.body)['jwt']).not_to be_nil
+      end
+    end
+  end
+end

041f67793c07b41b5110359393bcbabb9fd3e37a
Kyle Thompson <kyle@kylekthompson.com>
2017-08-08 23:47:11 -0700

    remove signin (outdated)

--- client/src/scenes/Header/container.ts
+++ client/src/scenes/Header/container.ts
@@ -1,17 +1,13 @@
 import Header from './Header';
 
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
-import { requestSignOut } from '../../services/store/Authentication/actionCreators';
 import { IApplicationState } from '../../services/store/types';
 
 const mapStateToProps = (state: IApplicationState) => ({
   isSignedIn: state.authentication.isSignedIn,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  requestSignOut: bindActionCreators(requestSignOut, dispatch),
-});
+const mapDispatchToProps = (_dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps, null, { pure: false })(Header);
--- client/src/scenes/SignIn/types.ts
+++
@@ -1,5 +0,0 @@
-export interface ISignInProps {
-  error: object;
-  isSignedIn: boolean;
-  requestSignIn: (email: string, password: string) => void;
-}

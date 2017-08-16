bca9c1ba677c755461040be804adb7b1e6d25c29
Kyle Thompson <kyle@kylekthompson.com>
2017-08-15 22:20:29 -0700

    allow addon items next to the inputs

--- client/src/components/Form/types.ts
+++ client/src/components/Form/types.ts
@@ -1,8 +1,10 @@
 export interface IInputProps extends React.ChangeTargetHTMLProps<HTMLInputElement> {
   asynchronousValidation?: (value: any) => Promise<string[]>;
   label?: string;
+  leftAddonItem?: React.ReactType;
   onChange: (event: React.FormEvent<HTMLInputElement>) => void;
   onValidation: (isValid: boolean) => void;
+  rightAddonItem?: React.ReactType;
   serverErrors?: string[];
   synchronousValidation?: (value: any) => string[];
 }

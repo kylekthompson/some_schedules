92ea44b70124def61ddd41a25647bbf38fa88d05
Kyle Thompson <kyle@kylekthompson.com>
2017-08-24 19:31:47 -0700

    add loading of my companies

--- client/src/scenes/Companies/scenes/MyCompanies/MyCompanies.tsx
+++ client/src/scenes/Companies/scenes/MyCompanies/MyCompanies.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 
+import Failure from '../../../../components/Failure';
+import Loading from '../../../../components/Loading';
 import { IMyCompaniesProps } from './types';
 
-const MyCompanies = ({ companies }: IMyCompaniesProps) => (
-  <div>
-    {companies.map((company) => <p key={company.id}>{company.name}</p>)}
-  </div>
-);
+class MyCompanies extends React.PureComponent<IMyCompaniesProps, {}> {
+  public componentDidMount() {
+    this.props.requestCompaniesByUserId(this.props.userId);
+  }
+
+  public render() {
+    const { companies, requestCompaniesByUserIdLoadingState } = this.props;
+
+    if (requestCompaniesByUserIdLoadingState.isLoading()) {
+      return <Loading message="Loading yours companies..." />;
+    } else if (requestCompaniesByUserIdLoadingState.isFailure()) {
+      return <Failure errors={requestCompaniesByUserIdLoadingState.errors()} />;
+    }
+
+    return (
+      <div>
+        {companies.map((company) => <p key={company.id}>{company.name}</p>)}
+      </div>
+    );
+  }
+}
 
 export default MyCompanies;

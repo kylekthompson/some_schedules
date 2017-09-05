a54c3d93eafa16b7677229f693bf99964eb7e252
Kyle Thompson <kyle@kylekthompson.com>
2017-09-05 12:28:38 -0400

    add spec for CompanyType

--- spec/graphql/types/objects/user_type_spec.rb
+++ spec/graphql/types/objects/user_type_spec.rb
@@ -11,4 +11,6 @@ RSpec.describe Types::Objects::UserType, type: :model do
   it { is_expected.to have_field(:firstName).that_returns_type(GraphQL::STRING_TYPE) }
   it { is_expected.to have_field(:lastName).that_returns_type(GraphQL::STRING_TYPE) }
   it { is_expected.to have_field(:token).that_returns_type(GraphQL::STRING_TYPE) }
+  it { is_expected.to have_field(:companies).that_returns_type(Types::Objects::CompanyType.connection_type) }
+  it { is_expected.to have_field(:companyUsers).that_returns_type(Types::Objects::CompanyUserType.connection_type) }
 end

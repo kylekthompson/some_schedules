ee91e69cbc2db6893c03253b1c9d9b63fd497f64
Kyle Thompson <kyle@kylekthompson.com>
2017-09-07 11:35:19 -0400

    add spec for have a top-level token query (for signing in)

--- spec/graphql/resolvers/user/token_spec.rb
+++ spec/graphql/resolvers/user/token_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Resolvers::User::Token, type: :model do
   subject(:resolver) { described_class }
 
   let(:ctx) { { current_user: current_user } }
-  let(:args) { { password: password } }
+  let(:args) { { email: email, password: password } }
   let(:actual_password) { 'password' }
   let(:obj) { create(:user, password: actual_password, password_confirmation: actual_password) }
   let(:result) { resolver.call(obj, args, ctx) }
@@ -19,6 +19,7 @@ RSpec.describe Resolvers::User::Token, type: :model do
 
   context 'when the correct password is provided' do
     let(:current_user) { nil }
+    let(:email) { nil }
     let(:password) { actual_password }
 
     specify { expect(result).not_to be_nil }
@@ -31,6 +32,7 @@ RSpec.describe Resolvers::User::Token, type: :model do
 
   context 'when an incorrect password is provided' do
     let(:current_user) { nil }
+    let(:email) { nil }
     let(:password) { 'abc' }
 
     specify { expect(result).to be_nil }
@@ -38,6 +40,7 @@ RSpec.describe Resolvers::User::Token, type: :model do
 
   context 'when the current_user is the user requested and no password is provided' do
     let(:current_user) { obj }
+    let(:email) { nil }
     let(:password) { nil }
 
     specify { expect(result).not_to be_nil }
@@ -50,7 +53,29 @@ RSpec.describe Resolvers::User::Token, type: :model do
 
   context 'when the current_user is not the user requested and no password is provided' do
     let(:current_user) { nil }
+    let(:email) { nil }
     let(:password) { nil }
+
+    specify { expect(result).to be_nil }
+  end
+
+  context 'when a matching email and password are provided' do
+    let(:result) { resolver.call(nil, args, ctx) }
+    let(:current_user) { nil }
+    let(:email) { obj.email }
+    let(:password) { actual_password }
+
+    it 'actually gets a token' do
+      result
+      expect(token).to have_received(:token)
+    end
+  end
+
+  context 'when an email and incorrect password are provided' do
+    let(:result) { resolver.call(nil, args, ctx) }
+    let(:current_user) { nil }
+    let(:email) { obj.email }
+    let(:password) { 'abc' }
 
     specify { expect(result).to be_nil }
   end
--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Types::QueryType, type: :model do
   it { is_expected.to have_field(:companyUsers).that_returns_type(Types::Objects::CompanyUserType) }
   it { is_expected.to have_field(:user).that_returns_type(Types::Objects::UserType) }
   it { is_expected.to have_field(:users).that_returns_type(Types::Objects::UserType) }
+  it { is_expected.to have_field(:token).that_returns_type(GraphQL::STRING_TYPE) }
   it { is_expected.to have_field(:node).that_returns_type(GraphQL::Relay::Node.interface) }
   it { is_expected.to have_field(:nodes).that_returns_type(GraphQL::Relay::Node.interface) }
 end

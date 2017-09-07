5dad8c835f390668edeaae2c01106fd6664f6c80
Kyle Thompson <kyle@kylekthompson.com>
2017-09-07 11:35:55 -0400

    add top-level token field

--- app/graphql/resolvers/user/token.rb
+++ app/graphql/resolvers/user/token.rb
@@ -5,18 +5,26 @@ module Resolvers
     module Token
       class << self
         def call(obj, args, ctx)
-          return token(obj) if ctx[:current_user]&.id == obj.id
-          return token(obj) if password_is_correct(obj, args[:password])
+          return token(obj) if obj.present? && ctx[:current_user]&.id == obj.id
+          return token(obj) if obj.present? && password_matches_obj?(obj, args[:password])
+          return token_for_args(args[:email], args[:password]) if args[:email].present?
           nil
         end
 
         private
 
+        def token_for_args(email, password)
+          return nil unless email.present?
+          user = ::User.find_by(email: email)
+          return token(user) if user&.authenticate(password)
+          nil
+        end
+
         def token(user)
           Knock::AuthToken.new(payload: user.to_token_payload).token
         end
 
-        def password_is_correct(user, password)
+        def password_matches_obj?(user, password)
           return false unless user.present?
           return true if user.authenticate(password)
           false

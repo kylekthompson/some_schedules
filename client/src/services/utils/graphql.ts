24b32f98691513c4bbf76bf08d5ae65efed853f9
Kyle Thompson <kyle@kylekthompson.com>
2017-09-28 15:19:48 -0400

    add the graphql client and set it up with the auth header

---
+++ client/src/services/utils/graphql.ts
@@ -0,0 +1,24 @@
+import Cache from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import HttpLink from 'apollo-link-http';
+import SetContextLink from 'apollo-link-set-context';
+
+import { getToken } from './authentication';
+
+const uri = '/api/v1/graphql';
+const setContext = (context) => ({
+  ...context,
+  headers: {
+    ...context.headers,
+    Authorization: `Bearer ${getToken()}`,
+  },
+});
+
+const cache = new Cache();
+const link = ApolloLink.from([
+  new SetContextLink(setContext),
+  new HttpLink({ uri }),
+]);
+
+export const client = new ApolloClient({ cache, link });

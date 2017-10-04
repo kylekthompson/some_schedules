3b2f7cd73312226e01c942df4a41da13804f16bc
Kyle Thompson <kyle@kylekthompson.com>
2017-10-04 12:10:40 -0400

    loading and failure routes aren't used after switching to GraphQL

--- client/src/components/FailureRoute/types.ts
+++
@@ -1,7 +0,0 @@
-import { RouteProps } from 'react-router-dom';
-
-import { IErrors } from '../../services/graphql/types';
-
-export interface IFailureRouteProps extends RouteProps {
-  errors: IErrors;
-}

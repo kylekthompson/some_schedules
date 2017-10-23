1251375303b1e0b8b54a634d848aeb80de67172b
Kyle Thompson <kyle@kylekthompson.com>
2017-10-22 20:49:05 -0400

    add a flyout container for the day picker component

--- client/src/scenes/Schedule/components/WeeklyCalendar/components/Navigation/index.tsx
+++ client/src/scenes/Schedule/components/WeeklyCalendar/components/Navigation/index.tsx
@@ -1,15 +1,67 @@
 import * as React from 'react';
 
-import { FlexChild } from '../../../../../../components/Flex';
+import styled, { css } from 'styled-components';
 
-import { INavigationProps } from './types';
+import { FlexContainer } from '../../../../../../components/Flex';
+import { INavigationProps, INavigationState } from './types';
 
-const Navigation = (_props: INavigationProps) => (
-  <FlexChild
-    alignSelf="flex-end"
-  >
-    <button>&lt;</button> ----- <button>&gt;</button>
-  </FlexChild>
-);
+const DayPickerContainer = styled.div`
+  position: relative;
+`;
+
+const DayPicker = styled.div`
+  background-color: lightgrey;
+  height: 200px;
+  left: 50%;
+  margin-left: -100px;
+  position: absolute;
+  text-align: center;
+  top: 40px;
+  width: 200px;
+
+  &:before {
+    border-color: transparent transparent lightgrey transparent;
+    border-style: solid;
+    border-width: 10px;
+    content: '';
+    display: block;
+    left: 50%;
+    margin-left: -10px;
+    position: absolute;
+    top: -20px;
+  }
+
+  ${({ visible }: { visible?: boolean}) => !visible && css`display: none` || ''}
+`;
+
+class Navigation extends React.Component<INavigationProps, INavigationState> {
+  public state: INavigationState = {
+    isDayPickerVisible: false,
+  };
+
+  public render() {
+    const { isDayPickerVisible } = this.state;
+
+    return (
+      <FlexContainer
+        alignSelf="flex-end"
+        flexDirection="row"
+      >
+        <button>&lt;</button>
+        <DayPickerContainer>
+          <button onClick={this.toggleVisibility}>CAL</button>
+          <DayPicker visible={isDayPickerVisible}>CALENDAR GOES HERE</DayPicker>
+        </DayPickerContainer>
+        <button>&gt;</button>
+      </FlexContainer>
+    );
+  }
+
+  private toggleVisibility = () => {
+    this.setState((prevState) => ({
+      isDayPickerVisible: !prevState.isDayPickerVisible,
+    }));
+  }
+}
 
 export default Navigation;
--- client/src/scenes/Schedule/components/WeeklyCalendar/components/Navigation/types.ts
+++ client/src/scenes/Schedule/components/WeeklyCalendar/components/Navigation/types.ts
@@ -3,3 +3,7 @@ import { Moment } from 'moment-timezone';
 export interface INavigationProps {
   startOfWeek: Moment;
 }
+
+export interface INavigationState {
+  isDayPickerVisible: boolean;
+}

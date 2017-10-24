c750c5a49dfaf803861238f98dd455437c49f37f
Kyle Thompson <kyle@kylekthompson.com>
2017-10-24 15:04:43 -0400

    move week and day rendering to helper functions

--- client/src/scenes/Schedule/components/DayPicker/components/Picker/index.tsx
+++ client/src/scenes/Schedule/components/DayPicker/components/Picker/index.tsx
@@ -7,26 +7,47 @@ import Day from '../Day';
 import PickerWrapper from '../PickerWrapper';
 import { IPickerProps } from './types';
 
+const renderDays = (
+  currentMonth: moment.Moment,
+  index: number,
+  iteratingDay: moment.Moment,
+  onDayPick: (day: moment.Moment) => () => void,
+  selectedDay: moment.Moment
+) => moment.weekdaysShort().map((weekday) => {
+  const day = iteratingDay.add(1, 'day').clone();
+  return (
+    <Day
+      key={`${index}-${weekday}`}
+      currentMonth={currentMonth}
+      day={day}
+      onClick={onDayPick(day)}
+      selectedDay={selectedDay}
+    />
+  );
+});
+
+const renderWeek = (
+  currentMonth: moment.Moment,
+  index: number,
+  iteratingDay: moment.Moment,
+  onDayPick: (day: moment.Moment) => () => void,
+  selectedDay: moment.Moment
+) => (
+  <FlexContainer key={index} flex="1" flexDirection="row">
+    {renderDays(currentMonth, index, iteratingDay, onDayPick, selectedDay)}
+  </FlexContainer>
+);
+
 const Picker = ({ currentMonth, onDayPick, onMonthChange, selectedDay, visible }: IPickerProps) => {
   const iteratingDay = currentMonth.clone().startOf('month').startOf('week').subtract(1, 'day');
   const lastDay = currentMonth.clone().endOf('month').endOf('week');
 
-  const weekRows = new Array(lastDay.diff(iteratingDay, 'week')).fill(0).map((_, index) => (
-    <FlexContainer key={index} flex="1" flexDirection="row">
-      {moment.weekdaysShort().map((weekday) => {
-        const day = iteratingDay.add(1, 'day').clone();
-
-        return (
-          <Day
-            key={`${index}-${weekday}`}
-            currentMonth={currentMonth}
-            day={day}
-            onClick={onDayPick(day)}
-            selectedDay={selectedDay}
-          />
-        );
-      })}
-    </FlexContainer>
+  const weekRows = new Array(lastDay.diff(iteratingDay, 'week')).fill(0).map((_, index) => renderWeek(
+    currentMonth,
+    index,
+    iteratingDay,
+    onDayPick,
+    selectedDay
   ));
 
   return (

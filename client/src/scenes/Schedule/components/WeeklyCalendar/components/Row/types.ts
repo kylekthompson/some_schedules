837416d6c32ee1fc9018151b46defb6ad43f1855
Kyle Thompson <kyle@kylekthompson.com>
2017-10-26 12:18:21 -0400

    remove unused interface

--- client/src/scenes/Schedule/components/WeeklyCalendar/components/Row/types.ts
+++ client/src/scenes/Schedule/components/WeeklyCalendar/components/Row/types.ts
@@ -2,11 +2,6 @@ import { Moment } from 'moment-timezone';
 
 import { IUser } from '../../../../../../services/graphql/types';
 
-export interface IRowColumnProps {
-  previousDay: Moment;
-  user: IUser;
-}
-
 export interface IRowProps {
   startOfWeek: Moment;
   user: IUser;

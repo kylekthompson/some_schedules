b5e53ed9182412191b72c81f012d5a1da7d017cc
Kyle Thompson <kyle@kylekthompson.com>
2017-11-03 12:19:37 -0400

    add auth requirement specs to the rest of the finders

--- spec/graphql/resolvers/company/shift_finder_spec.rb
+++ spec/graphql/resolvers/company/shift_finder_spec.rb
@@ -13,13 +13,23 @@ RSpec.describe Resolvers::Company::ShiftFinder, type: :model do
   let(:company) { create(:company, :with_owner) }
   let!(:shift) { company.users.first.shifts.create(attributes_for(:shift)) }
   let(:arguments) { nil }
-  let(:context) { nil }
+  let(:context) { { current_user: current_user } }
 
   before do
     create(:company, :with_owner).users.first.shifts.create(attributes_for(:shift))
   end
 
-  it 'finds the shifts for a company' do
-    expect(result).to contain_exactly(shift)
+  context 'when there is a current user' do
+    let(:current_user) { company.users.first }
+
+    it 'finds the shifts for a company' do
+      expect(result).to contain_exactly(shift)
+    end
+  end
+
+  context 'when there is not a current user' do
+    let(:current_user) { nil }
+
+    specify { expect { result }.to raise_error(GraphQL::ExecutionError) }
   end
 end
--- spec/graphql/resolvers/company/user_finder_spec.rb
+++ spec/graphql/resolvers/company/user_finder_spec.rb
@@ -13,13 +13,23 @@ RSpec.describe Resolvers::Company::UserFinder, type: :model do
   let(:company) { create(:company) }
   let!(:user) { company.users.create(attributes_for(:user)) }
   let(:arguments) { nil }
-  let(:context) { nil }
+  let(:context) { { current_user: current_user } }
 
   before do
     create(:company, :with_owner)
   end
 
-  it 'finds the users for a company' do
-    expect(result).to contain_exactly(user)
+  context 'when there is a current user' do
+    let(:current_user) { user }
+
+    it 'finds the users for a company' do
+      expect(result).to contain_exactly(user)
+    end
+  end
+
+  context 'when there is not a current user' do
+    let(:current_user) { nil }
+
+    specify { expect { result }.to raise_error(GraphQL::ExecutionError) }
   end
 end

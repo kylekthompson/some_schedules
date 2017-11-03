872181370ac77cbadbb1adbdca22d80077530301
Kyle Thompson <kyle@kylekthompson.com>
2017-11-03 12:01:00 -0400

    require auth on user finding

--- app/graphql/resolvers/user/finder.rb
+++ app/graphql/resolvers/user/finder.rb
@@ -4,7 +4,8 @@ module Resolvers
   module User
     module Finder
       class << self
-        def call(_object, arguments, _context)
+        def call(_object, arguments, context)
+          return unless context[:current_user].present?
           Batch::RecordLoader.for(::User).load(arguments[:id]) if arguments[:id].present?
         end
       end

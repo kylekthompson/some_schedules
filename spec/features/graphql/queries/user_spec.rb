da091082f3368c6e93dd72001ded90b671d1e7d4
Kyle Thompson <kyle@kylekthompson.com>
2017-11-03 12:00:21 -0400

    add spec for finding users

--- spec/features/graphql/queries/user_spec.rb
+++ spec/features/graphql/queries/user_spec.rb
@@ -2,11 +2,11 @@
 
 require 'rails_helper'
 
-RSpec.describe 'query { viewer }' do
+RSpec.describe 'query { user }' do
   subject(:query) do
     <<~GRAPHQL
       query {
-        viewer {
+        user(id: #{id}) {
           id
         }
       }
@@ -22,17 +22,25 @@ RSpec.describe 'query { viewer }' do
   end
   let(:variables) { {} }
   let(:context) { { current_user: current_user } }
-  let(:current_user) { nil }
+  let(:current_user) { create(:user) }
+  let(:user) { create(:user) }
 
   context 'when there is a current user' do
-    let(:current_user) { create(:user) }
+    let(:id) { user.id }
 
-    specify { expect(result[:viewer][:id]).to eq(current_user.id) }
+    specify { expect(result[:user][:id]).to eq(id) }
+
+    context 'but there is no user for the provided id' do
+      let(:id) { (User.last&.id || 0) + 1000 }
+
+      specify { expect(result[:user]).to be_nil }
+    end
   end
 
   context 'when there is not a current user' do
     let(:current_user) { nil }
+    let(:id) { user.id }
 
-    specify { expect(result[:viewer]).to be_nil }
+    specify { expect(result[:user]).to be_nil }
   end
 end

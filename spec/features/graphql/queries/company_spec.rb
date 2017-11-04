33525c8fdb36029ac3cb966ea4cd956fd78c2244
Kyle Thompson <kyle@kylekthompson.com>
2017-11-03 22:08:03 -0400

    improve the feature specs for company queries

--- spec/features/graphql/queries/company_spec.rb
+++ spec/features/graphql/queries/company_spec.rb
@@ -7,48 +7,96 @@ RSpec.describe 'query { company }' do
     <<~GRAPHQL
       query {
         company(slug: "#{slug}") {
-          id
           slug
-          users {
-            id
-          }
-          shifts {
-            id
-          }
         }
       }
     GRAPHQL
   end
 
-  let(:result) do
+  let(:execution_result) do
     SomeSchedulesSchema.execute(
       query,
       variables: variables,
       context: context
-    ).with_indifferent_access[:data]
+    ).with_indifferent_access
   end
+  let(:data) { execution_result[:data] }
+  let(:errors) { execution_result[:errors] }
   let(:variables) { { slug: slug } }
   let(:context) { { current_user: current_user } }
-  let(:current_user) { nil }
+  let(:current_user) { create(:user) }
   let(:company) { create(:company) }
 
-  context 'when there is a current user' do
-    let(:current_user) { create(:user) }
+  describe 'authentication' do
     let(:slug) { company.slug }
 
-    specify { expect(result[:company][:slug]).to eq(slug) }
+    context 'when there is a current user' do
+      let(:current_user) { create(:user) }
 
-    context 'but there is no company for the provided slug' do
-      let(:slug) { super() + '-does-not-exist' }
+      specify { expect(data[:company][:slug]).to eq(slug) }
+      specify { expect(errors).to be_nil }
+    end
 
-      specify { expect(result[:company]).to be_nil }
+    context 'when there is not a current user' do
+      let(:current_user) { nil }
+
+      specify { expect(data[:company]).to be_nil }
+      specify { expect(errors.pluck(:message)).to include(match(/authentication/i)) }
     end
   end
 
-  context 'when there is not a current user' do
-    let(:current_user) { nil }
-    let(:slug) { company.slug }
+  describe 'querying for just the company' do
+    context 'when passed a slug that belongs to a company' do
+      let(:slug) { company.slug }
 
-    specify { expect(result[:company]).to be_nil }
+      specify { expect(data[:company][:slug]).to eq(slug) }
+      specify { expect(errors).to be_nil }
+    end
+
+    context 'when passed a slug that does not belong to a company' do
+      let(:slug) { 'does-not-exist' }
+
+      specify { expect(data[:company]).to be_nil }
+      specify { expect(errors).to be_nil }
+    end
+  end
+
+  describe 'querying for just the company and its relations' do
+    subject(:query) do
+      <<~GRAPHQL
+        query {
+          company(slug: "#{slug}") {
+            slug
+            users {
+              id
+            }
+            shifts {
+              id
+            }
+          }
+        }
+      GRAPHQL
+    end
+
+    let(:slug) { company.slug }
+    let!(:company_user) { company.users.create(attributes_for(:user)) }
+    let!(:company_shift) { company_user.shifts.create(attributes_for(:shift)) }
+
+    before do
+      create(:company, :with_owner).tap do |other_company|
+        other_company.users.first.shifts.create(attributes_for(:shift))
+      end
+    end
+
+    specify { expect(data[:company][:slug]).to eq(slug) }
+    specify { expect(errors).to be_nil }
+
+    it 'includes only users within the company' do
+      expect(data[:company][:users].pluck(:id)).to contain_exactly(company_user.id)
+    end
+
+    it 'includes only shifts within the company' do
+      expect(data[:company][:shifts].pluck(:id)).to contain_exactly(company_shift.id)
+    end
   end
 end

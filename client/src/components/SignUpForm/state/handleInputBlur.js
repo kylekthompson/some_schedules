35d2e47efb36568cc2aca5933929c5134dd41ff0
Kyle Thompson <kyle@kylekthompson.com>
2018-01-25 22:04:11 -0500

    actually remove bootstrap and fix form validation state

--- client/src/components/SignUpForm/state/handleInputBlur.js
+++ client/src/components/SignUpForm/state/handleInputBlur.js
@@ -3,10 +3,14 @@ import { formValuesFromState } from 'models/form';
 const handleInputBlur = (field, validations, event) => {
   const value = event.currentTarget.value;
   return (state) => {
-    const errors = validations[field].run({
-      ...formValuesFromState(state),
-      [field]: value,
-    });
+    let errors = [];
+
+    if (state.form[field].isDirty) {
+      errors = validations[field].run({
+        ...formValuesFromState(state),
+        [field]: value,
+      });
+    }
 
     return {
       ...state,

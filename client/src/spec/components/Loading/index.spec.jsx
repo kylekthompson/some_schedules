49bb0a82373dba4013411f4a257895bcdcd99491
Kyle Thompson <kyle@kylekthompson.com>
2018-01-25 22:38:25 -0500

    add specs for Loading

---
+++ client/src/spec/components/Loading/index.spec.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { mount } from 'enzyme';
+
+import Loading from 'components/Loading';
+
+const mountComponent = (props) => mount(
+  <Loading
+    message="message"
+    {...props}
+  />
+);
+
+describe('<Loading />', () => {
+  it('renders the loading message', () => {
+    const message = 'Loading...';
+    const wrapper = mountComponent({
+      message,
+    });
+
+    expect(wrapper.contains(message)).toEqual(true);
+  });
+});

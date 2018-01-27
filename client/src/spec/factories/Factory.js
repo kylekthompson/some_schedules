6f18b6369546911977bb8219064ae21e4189414d
Kyle Thompson <kyle@kylekthompson.com>
2018-01-27 17:39:00 -0500

    add debugging support and fix factories

--- client/src/spec/factories/Factory.js
+++ client/src/spec/factories/Factory.js
@@ -1,7 +1,9 @@
-export default class Factory {
+class Factory {
   constructor(base = {}) {
     Object.keys(base).forEach((field) => {
       this[field] = base[field];
     });
   }
 }
+
+export default Factory;
--- client/src/spec/factories/User.js
+++ client/src/spec/factories/User.js
@@ -1,10 +1,15 @@
 import Factory from 'spec/factories/Factory';
 
 class User extends Factory {
-  email = 'kyle@kylekthompson.com';
-  id = Math.floor(Math.random() * 100000000);
-  firstName = 'Kyle';
-  lastName = 'Thompson';
+  constructor(user = {}) {
+    super({
+      email: 'kyle@kylekthompson.com',
+      id: Math.floor(Math.random() * 100000000),
+      firstName: 'Kyle',
+      lastName: 'Thompson',
+      ...user,
+    });
+  }
 }
 
 export default User;
